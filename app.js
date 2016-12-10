2e22a8de8ab67c90f9b84c60be0681400b9be67a
jmpena <extreme01>
2016-12-10 17:53:38 -0400

    inicializamos express

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var Persona = require('./persona.js');
+
+app.get('/', function (req, res) {
+    var persona = new Persona('Jose', 'Pena');
+    console.log(persona.nombreCompleto());
+
+   res.send('Hello World');
+});
+
+app.post('/api/personas',function (req,res) {
+    // cPersona.nombre = req.body.nombre;
+    // cPersona.apellido = req.body.apellido;
+   res.send("");
+});
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+});
