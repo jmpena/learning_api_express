f3b95691450d9ed394ade4d164b70b490ea0602b
jmpena <extreme01>
2016-12-10 17:57:21 -0400

    prueba cerrando issue

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var Persona = require('./persona.js');
 
 app.get('/', function (req, res) {
-    var persona = new Persona('Jose', 'Pena');
+    var persona = new Persona('Jose', 'Pena Jeres');
     console.log(persona.nombreCompleto());
 
    res.send('Hello World');
